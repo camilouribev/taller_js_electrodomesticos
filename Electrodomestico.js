64a29edb3be3d42ada0385a9b94cac29ef986da0
Camilo U <cauribeva@unal.edu.co>
2021-09-06 10:44:49 -0500

    Inicio construcción de clases

---
+++ Electrodomestico.js
@@ -0,0 +1,56 @@
+class Electrodomestico {
+  constructor(consumo, esExtranjero) {
+    this.consumo = consumo;
+    this.esExtranjero = esExtranjero;
+    this.precio = 0;
+  }
+
+  setConsumo(consumo) {
+    this.consumo = consumo;
+  }
+
+  getConsumo() {
+    return this.consumo;
+  }
+
+  getEsExtranjero() {
+    return this.esExtranjero;
+  }
+
+  setPrecio(precio) {
+    this.precio = precio;
+  }
+
+  getPrecio() {
+    return this.precio;
+  }
+
+  calculoPrecio() {
+    switch (this.consumo) {
+      case "A":
+        this.precio += 450000;
+        break;
+
+      case "B":
+        this.precio += 350000;
+        break;
+
+      case "C":
+        this.precio += 250000;
+        break;
+    }
+
+    switch (this.esExtranjero) {
+      case false: {
+        this.precio += 250000;
+        break;
+      }
+      case true: {
+        this.precio += 350000;
+        break;
+      }
+    }
+
+    return this.precio;
+  }
+}
---
+++ Nevera.js
@@ -0,0 +1,5 @@
+import Electrodomestico from "./Electrodomestico";
+
+class Nevera extends Electrodomestico (consumo, esExtranjero){
+    constructor()
+}
