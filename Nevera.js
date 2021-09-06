4ba1972f6a5136287b6277ae5d6488a24a943c3e
Camilo U <cauribeva@unal.edu.co>
2021-09-06 10:57:13 -0500

    Creada clase nevera

--- Nevera.js
+++ Nevera.js
@@ -1,5 +1,30 @@
 import Electrodomestico from "./Electrodomestico";
 
-class Nevera extends Electrodomestico (consumo, esExtranjero){
-    constructor()
+class Nevera extends Electrodomestico {
+  constructor(consumo, esExtranjero, capacidad) {
+    super(consumo, esExtranjero);
+    this.capacidad = capacidad;
+  }
+  setCapacidad(capacidad) {
+    this.capacidad = capacidad;
+  }
+
+  getCapacidad() {
+    return this.capacidad;
+  }
+
+  recargoPorCapacidad() {
+    if (this.capacidad > 120) {
+      recargo = (this.capacidad - 120) / 10;
+      return this.precio * recargo * 0.05;
+    }
+  }
+
+  calcularPrecio() {
+    if (this.capacidad > 120) {
+      return super.calcularPrecio() + this.recargoPorCapacidad();
+    } else {
+      return super.calcularPrecio();
+    }
+  }
 }
